b8a7277ae43aea5559e23015b445f478aba7378c
Shaheb Ali <systembugbd@gmail.com>
2021-12-05 12:16:13 +0600

    readme file updated

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 ## POLL APP Using Nodejs Express and EJS Template engine
+
+## home
+
+<img src="public/assets/img/home.png" />
+
+Click Create New Poll
+
+## Create Poll
+
+<img src="public/assets/img/poll_create.png" />
+
+Click Poll Fillup form
+
+## Fillup Poll Form
+
+<img src="public/assets/img/poll_fillup.png" />
+
+After submition
+
+## Poll List
+
+<img src="public/assets/img/poll_list.png" />
+
+After go to list click on your poll
+
+## Poll Opinion Submission form
+
+<img src="public/assets/img/poll_submit.png" />
+
+Result in count and percentage
+
+## Poll Result
+
+<img src="public/assets/img/result.png" />
--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const path = require('path');
-const express = require('express');
-const morgan = require('morgan');
-const connectDB = require('./db');
-const mongoose = require('mongoose');
-const router = require('./routes/router');
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const connectDB = require("./db");
+const mongoose = require("mongoose");
+const router = require("./routes/router");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.set('view engine', 'ejs');
-app.set('views');
+app.set("view engine", "ejs");
+app.set("views");
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/', router);
+app.use("/", router);
 
 const serverAndDBConnect = async () => {
   await connectDB(process.env.MONGO_URI);
